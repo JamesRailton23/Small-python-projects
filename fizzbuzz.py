34ed9654869aa0a2028e6ba675faa9c25258dbc5
James Railton <90464294+JamesRailton23@users.noreply.github.com>
2023-08-31 22:27:30 +0100

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizbuzz():
+    for number in range(1,101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 5 == 0:
+            print("Buzz")
+        elif number % 3 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+
+fizbuzz()
